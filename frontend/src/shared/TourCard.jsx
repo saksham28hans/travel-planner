c873c2d53e68af5df692e4e54aef027d605bb253
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2023-07-31 01:02:57 +0530

    done

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from 'react';
-import React, { useContext, useState } from 'react';
 import { Card, CardBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './TourCard.css';
