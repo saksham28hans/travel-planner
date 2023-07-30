3e2edfc19cf80aa575609c05bf0b4b321b878105
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2023-07-30 10:33:52 +0530

    Updated Carousel

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -1,46 +1,71 @@
-import React,{useState,useEffect} from 'react'
-import Carousel from 'react-bootstrap/Carousel';
-import "./Caro.css"
-import heroImg from '../../assets/images/hero-img01.jpg'
-import heroImg02 from '../../assets/images/hero-img02.jpg'
-  
+import React, { useState, useEffect } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "./Caro.css";
+import heroImg from "../../assets/images/hero-img01.jpg";
+import heroImg02 from "../../assets/images/hero-img02.jpg";
 
 const Carousels = () => {
-    const [index, setIndex] = useState(0);
-    const slides = [
-      { id: 1, imageSrc: heroImg, altText: 'First slide' },
-      { id: 2, imageSrc: heroImg02, altText: 'Second slide' },
-      // Add more slides as needed
-    ];
-    const interval = 2000; // Switch every 3 seconds (adjust as needed)
-  
-    const handleSelect = (selectedIndex) => {
-      setIndex(selectedIndex);
-    };
-  
-    useEffect(() => {
-      // Automatically switch the carousel slides in a loop
-      const timer = setInterval(() => {
-        setIndex((prevIndex) => (prevIndex + 1) % slides.length);
-      }, interval);
-  
-      // Clear the timer when the component unmounts
-      return () => clearInterval(timer);
-    }, [interval, slides.length]);
-  
-    return (
-      <Carousel activeIndex={index} onSelect={handleSelect} className="main">
-        {slides.map((slide) => (
-          <Carousel.Item key={slide.id}>
+  const [index, setIndex] = useState(0);
+  const slides = [
+    { id: 1, imageSrc: heroImg, altText: "First slide" },
+    { id: 2, imageSrc: heroImg02, altText: "Second slide" },
+    // Add more slides as needed
+  ];
+  const interval = 2000;
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  // useEffect(() => {
+  //   // Automatically switch the carousel slides in a loop
+  //   const timer = setInterval(() => {
+  //     setIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  //   }, interval);
+
+  //   // Clear the timer when the component unmounts
+  //   return () => clearInterval(timer);
+  // }, [interval, slides.length]);
+
+  return (
+    <Carousel
+      activeIndex={index}
+      onSelect={handleSelect}
+      className="main"
+      controls={false}
+    >
+      {slides.map((slide) => (
+        <Carousel.Item key={slide.id}>
+          <div
+            style={{
+              position: "relative",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                width: 400,
+                background: "hsla(0, 0%, 0%, 0.5)",
+                color: "#fff",
+                padding: 10,
+                lineHeight: 1.8,
+                marginLeft: 10
+              }}
+            >
+             Travel World with Us.
+            </div>
             <img
               className="d-block w-100 he"
               src={slide.imageSrc}
               alt={slide.altText}
             />
-          </Carousel.Item>
-        ))}
-      </Carousel>
-    );
-  };
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
 
-export default Carousels
+export default Carousels;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,87 +1,83 @@
-import React, { useState, useEffect,useContext } from 'react';
-import { Container,Row,Button } from 'reactstrap';
-import { NavLink, Link, useNavigate } from 'react-router-dom';
-import logo from '../../assets/images/logo.png';
-import './header.css';
-import { AuthContext } from '../../context/authContext/AuthContext';
-import { logout } from '../../context/authContext/AuthActions';
+import React, { useState, useEffect, useContext } from "react";
+import { Container, Row, Button } from "reactstrap";
+import { NavLink, Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import "./header.css";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import { logout } from "../../context/authContext/AuthActions";
 const nav_links = [
   {
-    path: '/home',
-    display: 'Home',
+    path: "/home",
+    display: "Home",
   },
   {
-    path: '/about',
-    display: 'About',
+    path: "/about",
+    display: "About",
   },
   {
-    path: '/tours',
-    display: 'My Tours',
+    path: "/tours",
+    display: "My Tours",
   },
 ];
 const Header = () => {
-  const { user,dispatch } = useContext(AuthContext);
+  const { user, dispatch } = useContext(AuthContext);
   const navigate = useNavigate();
   // const logout = () => {
 
   //   navigate('/');
   // };
-  
-  useEffect(() => {
 
-  });
+  useEffect(() => {});
 
   return (
     <header className="header">
       <Container>
-        <Row>
-          <div className="nav_wrapper d-flex align-items-center justify-content-between">
-            <div className="logo">
-              <img src={logo} alt="" />
-            </div>
-            <div className="navigation">
-              <ul className="menu d-flex align-items-center gap-5">
-                {nav_links.map((item, index) => (
-                  <li className="nav__item" key={index}>
-                    <NavLink
-                      to={item.path}
-                      className={(navClass) =>
-                        navClass.isActive ? 'active__link' : ''
-                      }
-                    >
-                      {item.display}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="nav__right d-flex align-items-center gap-4">
-              <div className="nav__btns d-flex align-item-center gap-4">
-                {false ? (
-                  <>
-                    <h5 style={{margin:'auto'}}>{user.username}</h5>
-                    <Button className="btn btn-dark" onClick={()=>dispatch(logout())}>
-                      Logout
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    <Button className="btn primary__btn">
-                      <Link to="/login">Login</Link>
-                    </Button>
-                    <Button className="btn primary__btn">
-                      <Link to="/register">Register</Link>
-                    </Button>
-                  </>
-                )}
-              </div>
-              <span className="mobile__menu">
-                <i class="ri-menu-line"></i>
-              </span>
+        <div className="nav_wrapper d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="navigation">
+            <ul className="menu d-flex align-items-center gap-5">
+              {nav_links.map((item, index) => (
+                <li className="nav__item" key={index}>
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive ? "active__link" : ""
+                    }
+                  >
+                    {item.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="nav__right d-flex align-items-center gap-4">
+            <div className="nav__btns d-flex align-item-center gap-4">
+              {false ? (
+                <>
+                  <h5 style={{ margin: "auto" }}>{user.username}</h5>
+                  <Button
+                    className="btn btn-dark"
+                    onClick={() => dispatch(logout())}
+                  >
+                    Logout
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button className="btn primary__btn">
+                    <Link to="/login">Login</Link>
+                  </Button>
+                  <Button className="btn primary__btn">
+                    <Link to="/register">Register</Link>
+                  </Button>
+                </>
+              )}
             </div>
           </div>
-            </Row>
-          </Container>
+        </div>
+      </Container>
     </header>
   );
 };
